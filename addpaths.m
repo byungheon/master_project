d97cd98253e07c142372f4f581327ad40fcf861e
DESKTOP-MFG0E6F\BH <allbyungheon@gmail.com>
2018-10-26 23:30:23 +0900

    file generation has been fixed

--- addpaths.m
+++ addpaths.m
@@ -3,7 +3,7 @@ addpath(genpath('apps/'));
 addpath(genpath('models/'));
 addpath(genpath('scenarios'));
 addpath('base','control','doc','gp','loss','test','util','practice_note');
-if ~exist('data','dir')
+if exist('data','dir') == 7
    mkdir('data');
 end
 addpath('data');
