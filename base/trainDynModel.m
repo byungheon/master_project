f45c77116ec9000137bbbf82d94dc87b077eeba0
DESKTOP-MFG0E6F\BH <allbyungheon@gmail.com>
2018-10-29 23:57:10 +0900

    marginalize gp dynamics by ode45

--- base/trainDynModel.m
+++ base/trainDynModel.m
@@ -20,7 +20,34 @@ xaug = [xx(:,dyno) xx(:,end-Du-2*Da+1:end-Du)];     % x augmented with angles
 inputs_temp = [xaug(:,dyni) xx(:,end-Du+1:end)];     % use dyni and ctrl
 targets_temp = yy(:,dyno);
 targets_temp(:,difi) = targets_temp(:,difi) - xx(:,dyno(difi));
+
+% marginalize gp dynamics
 joint_temp = xaug(:,[jointi length(jointi) + jointi]);
+
+if (isfield(dynmodel,'model') && ~strcmp(dynmodel.model,'PILCO'))
+    local.dynamics    = @dynamics_kp_nop;
+    local.OPTIONS     = odeset('RelTol', 1e-3, 'AbsTol', 1e-3);
+    local.ctrlfcn     = str2func('zoh_local');   
+    local.par.dt      = dt; local.par.delay = 0; local.par.tau = dt;
+    local.u0          = cell(1,length(jointi));
+    local.delta       = zeros(1,length(difi));
+    for i = 1:size(targets_temp,1)
+        local.tau = inputs_temp(i,end-Du+1:end)';
+        
+        for ii = 1:length(jointi), local.u0{ii} = @(t)local.ctrlfcn(local.tau(ii,:),t,local.par); end
+        [~, local.y] = ode45(local.dynamics, [0 dt/2 dt], joint_temp(i,:)', local.OPTIONS, local.u0{:});
+        
+        local.qdotdelt  = local.y(3,jointi) - joint_temp(i,jointi);
+        local.qddotdelt = local.y(3,jointi + length(jointi)) - joint_temp(i,jointi + length(jointi));
+       
+        local.delta(plant.jointi)           = local.qdotdelt;
+        local.delta(jointi+length(jointi))  = local.qddotdelt;
+        targets_temp(i,:)                   = targets_temp(i,:) - local.delta;
+    end
+    clear local;
+end
+
+% random sampling from gathered dynamics data
 if size(targets_temp,1) > 100
    disp('Random Sampling from obtained dynamic data');
    in_list = randsample(size(targets_temp,1),100);
@@ -29,12 +56,12 @@ x(end - initial_length +1:end,:) = [];
 y(end - initial_length +1:end,:) = [];
 xx = xx(in_list,:);
 yy = yy(in_list,:);
-joint_temp = joint_temp(in_list,:);
 x = [x;xx];
 y = [y;yy];
 targets_temp    = targets_temp(in_list,:);
 inputs_temp     = inputs_temp(in_list,:);
 
+% augment current dynamics data to previous data
 disp(['Obtained Dynamics Data: ' num2str(length(in_list)) '/' num2str(initial_length)]);
 if isfield(dynmodel, 'inputs')
     dynmodel.inputs     = [dynmodel.inputs;inputs_temp];
@@ -44,16 +71,7 @@ else
     dynmodel.targets    = targets_temp;
 end
 
-
-if (isfield(dynmodel,'model') && ~strcmp(dynmodel.model,'PILCO'))
-    delta           = zeros(size(dynmodel.targets));
-    delta(:,plant.jointi)    = joint_temp(:,end-length(jointi)+1:end) * dt;
-    for i = 1:size(dynmodel.inputs,1)
-        delta(i,plant.jointi+length(plant.jointi)) = dt * solveForwardDynamics(dynmodel.robot.A,dynmodel.robot.M,joint_temp(i,1:length(jointi))',joint_temp(i,end-length(jointi)+1:end)',dynmodel.inputs(i,end-Du+1:end)',dynmodel.robot.G, dynmodel.Vdot0, dynmodel.robot.F);
-    end
-    dynmodel.targets = dynmodel.targets - delta;
-end
-
+% train dynamics data
 dynmodel = dynmodel.train(dynmodel, plant, trainOpt);  %  train dynamics GP
 
 % display some hyperparameters
@@ -62,3 +80,16 @@ Xh = dynmodel.hyp;
 disp(['Learned noise std: ' num2str(exp(Xh(end,:)))]);
 % signal-to-noise ratios (values > 500 can cause numerical problems)
 disp(['SNRs             : ' num2str(exp(Xh(end-1,:)-Xh(end,:)))]);
+
+function u = zoh_local(f, t, par) % **************************** zero-order hold
+d = par.delay;
+if d==0
+                  u = f;
+else
+  e = d/100; t0=t-(d-e/2);
+  if t<d-e/2,     u=f(1);
+  elseif t<d+e/2, u=(1-t0/e)*f(1) + t0/e*f(2);    % prevents ODE stiffness
+  else            u=f(2);
+  end
+end
+end
