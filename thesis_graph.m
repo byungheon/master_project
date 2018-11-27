a643c6d2a0f6db1ee298dd530bee648477ac99ef
DESKTOP-MFG0E6F\BH <allbyungheon@gmail.com>
2018-11-27 17:26:22 +0900

    thesis graph revised

--- thesis_graph.m
+++ thesis_graph.m
@@ -5,23 +5,27 @@ if exist('plotting', 'var') && isfield(plotting, 'verbosity') ...
     && plotting.verbosity > 0
   if ~ishandle(3); figure(3); else set(0,'CurrentFigure',3); end
   clf(3); errorbar(0:dt:(H*dt),fantasy.mean{j},2*fantasy.std{j}); drawnow;
-  xlabel('time[s]'); ylabel('immediate cost');
+  xlabel('Time[s]','FontSize', 24); ylabel('Immediate cost','FontSize', 24);
 end
 hold on; plot(0:dt:((length(realCost{J+j})-1)*dt),realCost{J+j},'r','LineWidth',2); drawnow;
-
+lgd = legend('Predicted','Real');
+lgd.FontSize = 30;
 %% sequential cost graph
-number_pilco = 11;
-number_mine = 7;
+number_pilco = 10;
+number_mine = 6;
 pilco_cost = zeros(1,number_pilco);
 mine_cost = zeros(1,number_mine);
 for index = 1:number_pilco
-    name = 'C:\Users\BH\Desktop\졸논프로젝트\pilco_dynamics\data\Success\PILCO\pilco_0024_11trials_20_5\KukaSwingUp_0024_PILCO_' + string(index) + '_H200.mat';
+    name = 'C:\Users\BH\Desktop\졸논프로젝트\pilco_dynamics\data\Success\PILCO\pilco_5g_11085_12trials\KukaSwingUp_5g_11085_3313_PILCO_' + string(index) + '_H200.mat';
+%     name = 'C:\Users\BH\Desktop\졸논프로젝트\pilco_dynamics\data\Success\PILCO\pilco_5g_11085_12trials\KukaSwingUp_5g_11085_3313_PILCO_' + string(index) + '_H200.mat';
     load(name);
     pilco_cost(index) = sum(realCost{J+j});
 end
 
 for index = 1:number_mine
-    name = 'C:\Users\BH\Desktop\졸논프로젝트\pilco_dynamics\data\Success\MINE\12pmipmi_4_0024_7trials_20_5\KukaSwingUp_12pmipmi_4_0024_MINE_' + string(index) + '_H200.mat';
+    name = 'C:\Users\BH\Desktop\졸논프로젝트\pilco_dynamics\data\Success\MINE\mine_5g_11085_6trials\KukaSwingUp_5g_11085_3313_MINE_' + string(index) + '_H200.mat';
+%     name = 'C:\Users\BH\Desktop\졸논프로젝트\pilco_dynamics\data\Success\MINE\12pmipmi_4_0024_6trials_8_8\KukaSwingUp_12pmipmi_4_0024_088MINE_' + string(index) + '_H200.mat';
+
     load(name);
     mine_cost(index) = sum(realCost{J+j});
 end
@@ -30,11 +34,12 @@ figure(4);
 plot(1:number_pilco,pilco_cost,'b-o');
 hold on;
 plot(1:number_mine,mine_cost,'r-o');
-xlabel('Rollouts[#]'); ylabel('Summation of immediate cost');
-legend('PILCO','Ours');
+xlabel('Rollouts[#]','FontSize', 24); ylabel('Long-term cost','FontSize', 24);
+lgd = legend('PILCO','Ours');
+lgd.FontSize = 15;
 %% theta prediction graph
-close all;
-load('C:\Users\BH\Desktop\졸논프로젝트\pilco_dynamics\data\Success\MINE\12pmipmi_4_0024_7trials_20_5\KukaSwingUp_12pmipmi_4_0024_MINE_7_H200.mat');
+% close all;
+load('F:\bh\data\KukaSwingUp_12pmipmi_4_0024_MINE_8_H200.mat');
 % load('C:\Users\BH\Desktop\pilco_dynamics\data\Success\MINE\12pmipmi_4_0024_8trials_25_8\KukaSwingUp_12pmipmi_4_002_4_MINE_8_H200')
 
 q1_pred = M{j}(1,1:end-1);
@@ -49,22 +54,26 @@ figure(10);
 plot(0:dt:199*dt, q1_pred,'b','LineWidth',1.5);
 hold on;
 plot(0:dt:199*dt, q1,'r','LineWidth',1.5);
-xlabel('time[s]'); ylabel('Angle of the first joint [rad]');
-legend('predicted','real','Location','southeast');
+xlabel('Time[s]','FontSize', 18); ylabel('Angle of the first joint [rad]','FontSize', 18);
+lgd = legend('predicted','real','Location','southeast');
+lgd.FontSize = 30;
 
 figure(11);
 plot(0:dt:199*dt, q2_pred,'b','LineWidth',1.5);
 hold on;
 plot(0:dt:199*dt, q2,'r','LineWidth',1.5);
-xlabel('time[s]'); ylabel('Angle of the second joint [rad]');
-legend('predicted','real','Location','southeast');
+xlabel('Time[s]','FontSize', 18); ylabel('Angle of the second joint [rad]','FontSize', 18);
+lgd = legend('predicted','real','Location','southeast');
+lgd.FontSize = 30;
 
 figure(12);
 plot(0:dt:199*dt, q3_pred,'b','LineWidth',1.5);
 hold on;
 plot(0:dt:199*dt, q3,'r','LineWidth',1.5);
-xlabel('time[s]'); ylabel('Angle of the pendulum [rad]');
-legend('predicted','real','Location','southeast');
+xlabel('Time[s]','FontSize', 18); ylabel('Angle of the pendulum [rad]','FontSize', 18);
+lgd = legend('predicted','real','Location','southeast');
+lgd.FontSize = 30;
+
 %%
 clear all;
 clc;
